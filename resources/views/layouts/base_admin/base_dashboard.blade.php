32051abd760421bdf6fa46d3e6c23264d3f91755
adi94958 <117360640+adi94958@users.noreply.github.com>
2025-03-16 12:12:02 +0700

    f

--- resources/views/layouts/base_admin/base_dashboard.blade.php
+++ resources/views/layouts/base_admin/base_dashboard.blade.php
@@ -9,9 +9,9 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ secure_secure_asset('vendor/adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('vendor/adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ secure_secure_asset('vendor/adminlte3/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('vendor/adminlte3/css/adminlte.min.css') }}">
     @yield('script_head')
 </head>
 
